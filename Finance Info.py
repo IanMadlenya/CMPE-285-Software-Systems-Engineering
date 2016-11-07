96172c808c08a370f2ebe422d8ecc3a18cddd36a
Gaganjot Kaur Kang <gkang050@gmail.com>
2016-11-07 06:05:49 -0800

    Stock info

---
+++ Finance Info.py
@@ -0,0 +1,33 @@
+import subprocess
+import json
+import requests
+import time
+
+
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+ 
+def get_value(identifier):
+	get_value_url = 'http://finance.google.com/finance/info?client=ig&q=' + identifier 
+	value = subprocess.Popen(['curl', '-s', get_value_url], stdout=subprocess.PIPE).communicate()[0]
+	j = json.loads(value[5:len(value)-2])
+	return float(j['l'])
+ 
+if __name__ == "__main__":
+	stockname = raw_input("Please enter stalk symbol: ")
+	#inputformat =  'NASDAQ:AAPL'
+	print "USER INPUT: " + stockname
+	companycode = stockname.split(":")[1]
+	share_value = get_value(stockname)
+	companyname = get_symbol(companycode)
+	localtime = time.asctime( time.localtime(time.time()) )
+	print companyname
+	print localtime
+	print stockname 
+	print share_value
